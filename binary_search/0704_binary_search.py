c18eae0bcccd9562e86f3621aa1705cf96a61596
Jocelyn <biadr7342@gmail.com>
2026-04-11 17:09:22 +0800

    add binary solution

---
+++ binary_search/0704_binary_search.py
@@ -0,0 +1,14 @@
+def binary_search(nums,target):
+    left = 0
+    right = len(nums) - 1
+
+    while left <= right:
+        mid = (left+right) // 2
+        if nums[mid] == target:
+            return mid
+        elif nums[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+    return -1
+print(binary_search([-1,0,3,5,9,12],9))
