32544ebf7f02e48cb0273d38ac7d67c8f39da17c
Jocelyn <biadr7342@gmail.com>
2026-04-25 20:46:23 +0800

    fix rotated binary search

--- binary_search/0033_rotatedbinarySearch.py
+++ binary_search/0033_rotatedbinarySearch.py
@@ -11,15 +11,18 @@ def rotatedbinarySearch(nums, target):
         # 判断哪一半是完美有序的
         if nums[left] <= nums[mid]:
             # === 左半边是完美有序的 ===
-            # 【你的任务：在这里写代码】
-            # 提示：判断 target 是不是在 nums[left] 到 nums[mid] 之间？
-            # 如果在，移动 right；如果不在，移动 left。
+            if nums[left] <= target < nums[mid]:
+                right = mid-1 #target在左边，去左边找
+            else:
+                left = mid + 1 #target在右边，去右边找
             
         else:
             # === 右半边是完美有序的 ===
-            # 【你的任务：在这里写代码】
-            # 提示：判断 target 是不是在 nums[mid] 到 nums[right] 之间？
-            # 如果在，移动 left；如果不在，移动 right。
+            if nums[mid] < target <= nums[right]:
+                left = mid + 1
+            else:
+                right = mid - 1
             
     return -1
-    print(rotatedbinarySearch([4, 5, 6, 7, 0, 1, 2],0))
+
+print(rotatedbinarySearch([4, 5, 6, 7, 0, 1, 2],0))
